adce9936c995e9c351e74276d8b6f71a470500f8
vinay <iamvinaysinghrathore@gmail.com>
2020-07-18 01:12:11 +0530

    Stack Help

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 5000,function(){
 	console.log("the yelpcamp sever has started");
 })
