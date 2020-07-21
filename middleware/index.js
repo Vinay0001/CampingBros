2c473ea1a01da5fb06702497da278fef7e8385af
vinay <iamvinaysinghrathore@gmail.com>
2020-07-21 16:19:21 +0530

    add admin user

--- middleware/index.js
+++ middleware/index.js
@@ -8,18 +8,14 @@ middlewareObj.checkCGownership=function checkCGownership(req,res,next){
 	if(req.isAuthenticated()){
 
 	 Campground.findById(req.params.id,function(err,foundCampground){
-		if(err || !foundCampground){
-			req.flash("error","Campground not found");
-			res.redirect("back");
-		}else{
-					//does he owns the cg
-			if(foundCampground.author.id.equals(req.user._id)){
+		//does he owns the cg
+			if(foundCampground.author.id.equals(req.user._id)|| req.user.isAdmin){
 				next();
 			}else{
 				req.flash("error","You  don't have permission to do that");
 					res.redirect("back");			}
 		}
-	});
+	);
 	}else{
 		req.flash("error","You need to be Logged in to do that");
 		res.redirect("back");
@@ -31,17 +27,13 @@ middlewareObj.checkCommentOwnership=function checkCommentOwnership(req,res,next)
 	if(req.isAuthenticated()){
 
 	 Comment.findById(req.params.comment_id,function(err,foundComment){
-		if(err || !foundComment){
-			req.flash("error","Comment not found");
-			res.redirect("back");
-		}else{
-					//does he owns the comment
-			if(foundComment.author.id.equals(req.user._id)){
+				//does he owns the comment
+			if(foundComment.author.id.equals(req.user._id)|| req.user.isAdmin){
 				next();
 			}else{
 				req.flash("error","You don't have permission to do that");
 				res.redirect("back");			}
-		}
+		
 	});
 	}else{
 		req.flash("error","You need to be logged in to do that");
--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mongoose=require("mongoose");
 var passportLocalMongoose=require("passport-local-mongoose");
 var UserSchema=mongoose.Schema({
 	username:String,
-	password:String
+	password:String,
+	isAdmin:{type:Boolean,default:false}
 });
 
 UserSchema.plugin(passportLocalMongoose);
